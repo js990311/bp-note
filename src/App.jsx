2aa66926d0b165919ff6a7a56c92793fb2127aec
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-10-10 19:42:21 +0900

    [init]

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>BP-Note</h1>  
+      </div>
+      <Routes>
+      </Routes>
+    </>
+  )
+}
+
+export default App
