0797ad2b98810543fe0097da48e69bbf83bb28dc
Lee jong sik <102013392+js990311@users.noreply.github.com>
2024-10-18 21:32:24 +0900

    modify: tab component

--- src/components/global/tab/Tab.jsx
+++ src/components/global/tab/Tab.jsx
@@ -3,69 +3,30 @@ import TabTitle from './TabTitle';
 import TabContent from './TabContent';
 import './Tab.css';
 
-const Tab = () => {
+const Tab = ({titles, children, onDelete, onCreate}) => {
+    const [activeTab, setActiveTab] = useState(0);
     
-    const [tabInfo, setTabInfo] = useState(
-        {
-            activeTab : 0,
-            tabs : [
-                {
-                    title : 'tab1',
-                    content : 'tab1-content' 
-                },
-                {
-                    title : 'tab2',
-                    content : 'tab2-content' 
-                }
-            ]    
-        }
-    );
-    console.log(tabInfo);
     const onDeleteTab = (index) => {
-        console.log('delete');
-        setTabInfo((prev) => {
-            let alterTabs = prev.tabs.filter((tab, idx) => idx !== index);
-            let alterActiveTab = prev.activeTab === alterTabs.length ? prev.activeTab-1 : prev.activeTab; 
-            let alter = {
-                ...prev,
-                activeTab : alterActiveTab,
-                tabs : alterTabs,
-            }
-            return alter;
-        });
+        onDelete(index);
     }
 
     const onActiveTab = (index) => {
-        console.log('onClick');
-        setTabInfo({
-            ...tabInfo,
-            activeTab: index
-        });
+        setActiveTab(index);
     }
 
-    const onCreateTab = (newTab) => {
-        setTabInfo((prev) => {
-            let alterTabs = [
-                ...prev.tabs, 
-                newTab
-            ]
-            let alterActiveTab = alterTabs.length-1;
-            return {
-                activeTab : alterActiveTab,
-                tabs : alterTabs
-            }
-        });
+    const onCreateTab = () => {
+        onCreate();
     }
 
     return (
         <div className='tab'>
             <div className="tab-header">
                 {
-                    tabInfo.tabs.map((tab, idx)=>(
+                    titles.map((title, idx)=>(
                         <TabTitle 
                             key={idx}
-                            title={tab.title}
-                            isActive={tabInfo.activeTab === idx ? true : false}
+                            title={title}
+                            isActive={activeTab === idx ? true : false}
                             onActive={() => {onActiveTab(idx);}}
                             onDelete={()=>{onDeleteTab(idx)}}
                         />
@@ -74,10 +35,9 @@ const Tab = () => {
                 <button
                     onClick={
                         ()=>{
-                            onCreateTab( {
-                                title : 'newTabs',
-                                content : 'newTabs-content' 
-                            });
+                            onCreateTab( 
+
+                            );
                         }
                     }
                 >
@@ -86,12 +46,12 @@ const Tab = () => {
             </div>
             <div className="tab-body">
                 {
-                    tabInfo.tabs.map((tab, idx)=>(
+                    children.map((child, idx)=>(
                         <TabContent
                             key={idx}
-                            isActive={tabInfo.activeTab === idx ? true : false}
+                            isActive={activeTab === idx ? true : false}
                         >
-                            {tab.content}
+                            {child}
                         </TabContent>    
                     ))
                 }
--- src/components/record/form/BPrecordTab.jsx
+++ src/components/record/form/BPrecordTab.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import TabTitle from '../../global/tab/TabTitle';
-import TabContent from '../../global/tab/TabContent';
+import Tab from '../../global/tab/Tab';
 import '../../global/tab/Tab.css';
 import BPrecordForm from './BPrecordForm';
 
@@ -22,11 +21,16 @@ const BPrecordTab = () => {
         setPressures(newPressures);
     }
 
-    const [tabInfo, setTabInfo] = useState(
-        {
-            activeTab : 0,
-            tabs : [
-                ...pressures.map(
+    return (
+        <div className="record">
+            <Tab
+                titles={
+                    pressures.map((val, key) => (
+                        `${key}차 측정`
+                    ))
+                }
+            >
+                {pressures.map(
                     (pressure, index) => (
                         <BPrecordForm
                             key={index}
@@ -37,86 +41,8 @@ const BPrecordTab = () => {
                             }}
                         />
                     )
-                )
-            ]
-        }
-    );
-    console.log(tabInfo);
-    const onDeleteTab = (index) => {
-        console.log('delete');
-        setTabInfo((prev) => {
-            let alterTabs = prev.tabs.filter((tab, idx) => idx !== index);
-            let alterActiveTab = prev.activeTab === alterTabs.length ? prev.activeTab-1 : prev.activeTab; 
-            let alter = {
-                ...prev,
-                activeTab : alterActiveTab,
-                tabs : alterTabs,
-            }
-            return alter;
-        });
-    }
-
-    const onActiveTab = (index) => {
-        console.log('onClick');
-        setTabInfo({
-            ...tabInfo,
-            activeTab: index
-        });
-    }
-
-    const onCreateTab = (newTab) => {
-        setTabInfo((prev) => {
-            let alterTabs = [
-                ...prev.tabs, 
-                newTab
-            ]
-            let alterActiveTab = alterTabs.length-1;
-            return {
-                activeTab : alterActiveTab,
-                tabs : alterTabs
-            }
-        });
-    }
-
-    return (
-        <div className='tab'>
-            <div className="tab-header">
-                {
-                    tabInfo.tabs.map((tab, idx)=>(
-                        <TabTitle 
-                            key={idx}
-                            title={`${idx+1}차 측정`}
-                            isActive={tabInfo.activeTab === idx ? true : false}
-                            onActive={() => {onActiveTab(idx);}}
-                            onDelete={()=>{onDeleteTab(idx)}}
-                        />
-                    ))
-                }
-                <button
-                    onClick={
-                        ()=>{
-                            onCreateTab( {
-                                title : 'newTabs',
-                                content : 'newTabs-content' 
-                            });
-                        }
-                    }
-                >
-                    +
-                </button>
-            </div>
-            <div className="tab-body">
-                {
-                    tabInfo.tabs.map((tab, idx)=>(
-                        <TabContent
-                            key={idx}
-                            isActive={tabInfo.activeTab === idx ? true : false}
-                        >
-                            {tab}
-                        </TabContent>    
-                    ))
-                }
-            </div>
+                )}
+            </Tab>
         </div>
     );
 }
